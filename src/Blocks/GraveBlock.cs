04c9b9193d03a14e8796d54787d889bd7a909b9e
peterix <fork@kuelzer.me>
2020-04-10 09:36:04 +0200

    Add translation file and separate description

--- src/Blocks/GraveBlock.cs
+++ src/Blocks/GraveBlock.cs
@@ -1,13 +1,25 @@
-﻿using Vintagestory.API.Common;
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 
 namespace GraveMod.Blocks
 {
     public class GraveBlock : Block
     {
+        /// <summary>
+        /// This override is necessary because the HorizontalOrientable behaviour prevents the "drops" json-attribute from working 
+        /// </summary>
         public override ItemStack[] GetDrops(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1)
         {
             return null;
+        }
+
+        /// <summary>
+        /// This override is necessary because the HorizontalOrientable behaviour prevents the "drops" json-attribute from working 
+        /// </summary>
+        public override BlockDropItemStack[] GetDropsForHandbook(ItemStack handbookStack, IPlayer forPlayer)
+        {
+            return new BlockDropItemStack[0];
         }
     }
 }
